235dcaf9adad49b4c07cfe3e123945167b8734c3
TheYOSH <github.com@theyosh.nl>
2016-01-23 23:51:45 +0100

    Updated startup script for more stability

--- start.sh
+++ start.sh
@@ -1,42 +1,62 @@
 #!/bin/bash
 
+# Some settings
 RUN_AS_USER="pi"
 SCREEN_NAME="gekko"
-RESTART_TIME=15
-BASEDIR=$(dirname $(readlink -f $0))
+RESTART_TIME=10
+MAX_RESTARTS=5
+RESTART_TIMEOUT=45
+# End settings
+
+# Main program
+RESTART_ATTEMPTS=0
+BASEDIR=$(dirname $(readlink -nf $0))
+SCRIPT=$(basename $(readlink -nf $0))
 RUN=$1
 
+function message {
+  echo "$(date +"%d-%m-%Y %T"): $1"
+}
 
 if [ "${RUN}" == "run" ]
 then
-  RESTART_ATTEMPTS=0
-  SECONDS=0
   while true
   do
+    # Reset internal bash seconds counter
+    SECONDS=0
+
+    # Start terrarium software
+    message "Starting terrarium server software..."
     ${BASEDIR}/terrarium.py
 
-#    if test $SECONDS le 30
-#    then
-#      RESTART_ATTEMPTS=$((RESTART_ATTEMPTS+1))
-#    fi
+    # Crashed / stopped / something else...
+    if (( SECONDS < RESTART_TIMEOUT )); then
+      # Restarted to soon... counting
+      RESTART_ATTEMPTS=$((RESTART_ATTEMPTS+1))
+      message "Restart counter: ${RESTART_ATTEMPTS}"
 
-#    if test $RESTART_ATTEMPTS gt 10
-#    then
-#      echo "To manny errors, reboting"
-#      sync
-#      reboot
-#    fi
+      if (( RESTART_ATTEMPTS > MAX_RESTARTS )); then
+         # To many errors. Rebooting
+         message "To manny error (${RESTART_ATTEMPTS}). Final result is reboting!!!"
+         sync
+         reboot
+      fi
 
-    echo -n "Restarting in ${RESTART_TIME} seconds after ${SECONDS} seconds of running"
+    else
+      # Reset restart counter
+      RESTART_ATTEMPTS=0
+    fi
+
+    echo -n "$(date +"%d-%m-%Y %T"): Restarting in ${RESTART_TIME} seconds after running for ${SECONDS} seconds "
     for (( counter=1; counter<=${RESTART_TIME}; counter++ ))
     do
       echo -n "."
       sleep 1
     done
-    echo ""
+    echo " restart!"
   done
 else
-  echo "Starting terrarium server software... ${BASEDIR}"
+  message "Starting terrarium server software..."
   cd "${BASEDIR}"
-  screen -dmS ${SCREEN_NAME} su ${RUN_AS_USER} -c "./start.sh run"
+  screen -dmS ${SCREEN_NAME} su ${RUN_AS_USER} -c "./${SCRIPT} run"
 fi
