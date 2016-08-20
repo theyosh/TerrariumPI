4a503177ed9250830ae33deaf59037f581e90128
TheYOSH <github.com@theyosh.nl>
2016-08-20 22:12:31 +0200

    Fixed crashing threads with RPi.GPIO events

--- terrariumDoor.py
+++ terrariumDoor.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
+import thread
+
+from gevent import monkey, sleep
+monkey.patch_all()
 
 class terrariumDoor():
   
   def __init__(self, gpio_pin, callback = None):
     self.gpio_pin = int(gpio_pin)
-    self.door_status = 'closed'
+    self.door_status = None
     self.callback = callback
 
     ## set GPIO mode to BCM
     ## this takes GPIO number instead of pin number
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     
     ## use the built-in pull-up resistor
     GPIO.setup(self.gpio_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)  # activate input with PullUp
     
-    # Add detetion with callback
-    GPIO.add_event_detect(self.gpio_pin, GPIO.BOTH, callback=self.__update_status)
+    # Add detetion with callback THIS WILL CRASH THE GEVENT LOOP SOMEHOW!!!!!!
+    # GPIO.add_event_detect(gpio_pin, GPIO.BOTH, callback=callback, bouncetime=300)
+    thread.start_new_thread(self.__checker, ())
 
-  def __update_status(self):
-    current_status = 'open' if GPIO.input(self.__pin) else 'closed'
-    if current_status != self.door_status and self.callback:
-      self.callback(self.get_status())
-    
-    self.door_status = current_status
+  def __checker(self):
+    while True:
+      current_status = 'open' if GPIO.input(self.gpio_pin) else 'closed'
+      if current_status != self.door_status:
+        self.door_status = current_status
+        self.callback(True)
+
+      sleep(0.5)
 
   def get_status(self):
     return self.door_status
