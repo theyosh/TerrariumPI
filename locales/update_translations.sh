82fefb84be4245c11cc5a3dbfd13c892ed4a7001
TheYOSH <github.com@theyosh.nl>
2016-12-17 22:53:45 +0100

    Updated language file generation

--- locales/update_translations.sh
+++ locales/update_translations.sh
@@ -5,3 +5,35 @@ mv views/inc/footer.tpl views/inc/footer.tpl_tmp
 pygettext -v -a -n -o locales/terrariumpi.pot views/*.tpl views/inc/*.tpl *.py
 mv views/inc/footer.tpl_tmp views/inc/footer.tpl
 cd -
+
+for translation in `grep -r -h -o -e "_('[^)]\+')" ../views/*.tpl | sed "s/ /%20/g" | sort | uniq`; do
+  translation=${translation:3:-2}
+  translation=${translation//\%20/ }
+  if [ `grep -ic "${translation}" terrariumpi.pot` -eq 0 ]; then
+    echo "Adding missing ${translation}"
+    echo "#: Missing text string" >> terrariumpi.pot
+    echo "msgid \"${translation}\"" >> terrariumpi.pot
+    echo "msgstr \"\"" >> terrariumpi.pot
+    echo "" >> terrariumpi.pot
+  fi
+done
+
+sed -e 's@YEAR ORGANIZATION@2016 TheYOSH@g' \
+    -e 's@FIRST AUTHOR <EMAIL\@ADDRESS>, YEAR@Joshua (TheYOSH) Rubingh, <theyosh\@theyosh.nl>, 2016@g' \
+    -i terrariumpi.pot
+
+echo "Creating en_US language"
+NOW=`date "+%Y-%m-%d %H:%M%z"`
+grep -v 'msgstr ""' terrariumpi.pot | sed 's@msgid "\([^"]*\)"@msgid "\1"\nmsgstr "\1"@' > en_US/LC_MESSAGES/terrariumpi.po
+sed -e "s@YEAR-MO-DA HO:MI+ZONE@${NOW}@g" \
+    -e 's@FIRST AUTHOR@Joshua (TheYOSH) Rubingh@g' \
+    -e 's@ORGANIZATION@TheYOSH@g' \
+    -e 's@FULL NAME@Joshua (TheYOSH) Rubingh@g' \
+    -e 's@EMAIL\@ADDRESS@theyosh\@theyosh.nl@g' \
+    -e 's@CHARSET@UTF-8@g' \
+    -e 's@ENCODING@8bit@g' \
+    -e 's@PACKAGE VERSION@TerrariumPI 0.1@g' \
+    -e 's@"Language-Team: LANGUAGE <LL\@li.org>\\n"@"Language-Team: \\n"\n"Language: en_US\\n"@g' \
+    -i en_US/LC_MESSAGES/terrariumpi.po
+msgfmt en_US/LC_MESSAGES/terrariumpi.po -o en_US/LC_MESSAGES/terrariumpi.mp
+echo "Done!"
