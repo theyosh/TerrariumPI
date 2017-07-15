b8d04039590bac362595d8dac82f5088f23aa66b
TheYOSH <github.com@theyosh.nl>
2017-07-15 17:53:05 +0200

    Add js script for translations

--- locales/update_translations.sh
+++ locales/update_translations.sh
@@ -2,7 +2,7 @@
 
 cd ..
 mv views/inc/footer.tpl views/inc/footer.tpl_tmp
-pygettext -v -a -n -o locales/terrariumpi.pot views/*.tpl views/inc/*.tpl *.py
+pygettext -v -a -n -o locales/terrariumpi.pot static/js/terrariumpi.js views/*.tpl views/inc/*.tpl *.py
 mv views/inc/footer.tpl_tmp views/inc/footer.tpl
 cd -
 
