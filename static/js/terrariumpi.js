c5ddd8df1a6b5c0f5d890af01bf12c1e8a1c214d
TheYOSH <github.com@theyosh.nl>
2018-03-20 22:45:48 +0100

    Some finetuning. #125

--- static/js/terrariumpi.js
+++ static/js/terrariumpi.js
@@ -5,7 +5,6 @@ var globals = {
   connection: 'ws' + (location.protocol == 'https:' ? 's' : '') + '://' + location.host + '/live',
   temperature_indicator: 'C',
   distance_indicator: 'cm',
-  ph_indicator: 'PH',
   gauges: [],
   webcams: [],
   graphs: {},
@@ -1091,11 +1090,11 @@ function history_graph(name, data, type) {
           case 'average_distance':
             val = formatNumber(val) + ' ' + globals.distance_indicator;
             break;
-			
+
           case 'ph':
           case 'average_ph':
-            val = formatNumber(val) + ' ' + globals.ph_indicator;
-            break;			
+            val = formatNumber(val) + ' pH';
+            break;
 
           case 'switch':
             val = formatNumber(val) + ' W';
@@ -1114,7 +1113,7 @@ function history_graph(name, data, type) {
     case 'humidity':
     case 'temperature':
     case 'distance':
-	case 'ph':
+    case 'ph':
       graph_data = [{
         label: '{{_('Current')}}',
         data: data.current
