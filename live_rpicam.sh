fa380a42c6fb882b220b1d9b4db918d86d36bc41
TheYOSH <github.com@theyosh.nl>
2018-12-05 23:09:59 +0100

    Add resolution and rotations to live RPICam. #223

--- live_rpicam.sh
+++ live_rpicam.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-DIR=$1
+WIDTH=$1
+HEIGHT=$2
+ROTATION=$3
+DIR=$4
 
-raspivid -b 2000000 -o - -t 0 -w 1920 -h 1080 -fps 30 -g 30 -pf baseline -lev 4.1 | \
+if [ "{ROTATION}" == "h"];
+then
+  ROTATION="-hf"
+elif [ "{ROTATION}" == "v"];
+then
+  ROTATION="-vf"
+else
+then
+  ROTATION="-rot ${ROTATION}"
+fi
+
+raspivid -b 2000000 -o - -t 0 -w ${WIDTH} -h ${HEIGHT} ${ROTATION} -fps 30 -g 30 -pf main -lev 4.1 | \
  ffmpeg -nostdin -re -i - -c:v copy -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+split_by_time+program_date_time -hls_segment_filename "${DIR}/chunk_%03d.ts" "${DIR}/stream.m3u8"
