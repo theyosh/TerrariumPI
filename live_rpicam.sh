8b2256f875d4a6619e32ffaaf7732eb558c331d7
TheYOSH <github.com@theyosh.nl>
2018-12-06 22:47:49 +0100

    Add date time to live Raspicam

--- live_rpicam.sh
+++ live_rpicam.sh
@@ -15,5 +15,5 @@ else
   ROTATION="-rot ${ROTATION}"
 fi
 
-raspivid -b 2000000 -o - -t 0 -w ${WIDTH} -h ${HEIGHT} ${ROTATION} -fps 30 -g 30 -pf main -lev 4.1 | \
- ffmpeg -nostdin -re -i - -c:v copy -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+split_by_time+program_date_time -hls_segment_filename "${DIR}/chunk_%03d.ts" "${DIR}/stream.m3u8"
+raspivid -b 2000000 -o - -t 0 -w ${WIDTH} -h ${HEIGHT} ${ROTATION} -fps 30 -g 30 -pf main -lev 4.1 -a 12 | \
+ ffmpeg -nostdin -re -i - -c:v copy -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+split_by_time -hls_segment_filename "${DIR}/chunk_%03d.ts" "${DIR}/stream.m3u8"
