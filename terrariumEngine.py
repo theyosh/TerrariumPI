af51f42515c24dc7171b6a09d10c636a5e449756
TheYOSH <github.com@theyosh.nl>
2018-12-28 14:11:42 +0100

    Fix missing max diff value. #236

--- terrariumEngine.py
+++ terrariumEngine.py
@@ -193,11 +193,16 @@ class terrariumEngine(object):
         sensor.set_address(sensordata['address'])
 
       sensor.set_name(sensordata['name'])
-      sensor.set_max_diff(sensordata['max_diff'])
       sensor.set_alarm_min(sensordata['alarm_min'])
       sensor.set_alarm_max(sensordata['alarm_max'])
       sensor.set_limit_min(sensordata['limit_min'])
       sensor.set_limit_max(sensordata['limit_max'])
+
+      # TEMP
+      if 'max_diff' not in sensordata:
+        sensordata['max_diff'] = abs(sensor.get_limit_max() - sensor.get_limit_min()) * 0.25
+
+      sensor.set_max_diff(sensordata['max_diff'])
 
       if 'chirp' == sensor.get_type():
         if 'min_moist' in sensordata and sensordata['min_moist'] is not None:
