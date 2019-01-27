05b438d423e0d0b86581e6713eeec2c3f0cb2c34
TheYOSH <github.com@theyosh.nl>
2019-01-27 16:29:02 +0100

    Changed installation order for taking a cup of coffe ;)

--- install.sh
+++ install.sh
@@ -66,6 +66,82 @@ debconf-apt-progress -- apt-get -y update
 debconf-apt-progress -- apt-get -y full-upgrade
 debconf-apt-progress -- apt-get -y install libftdi1 screen git subversion watchdog build-essential i2c-tools pigpio owserver sqlite3 vlc-nox ffmpeg libfreetype6-dev libjpeg-dev libasound2-dev sispmctl lshw libffi-dev ntp libglib2.0-dev rng-tools libcblas3 libatlas3-base libjasper1 libgstreamer0.10-0 libgstreamer1.0-0 libilmbase12 libopenexr22 libgtk-3-0 libxml2-dev libxslt1-dev python-twisted $PYTHON_LIBS
 
+# Set the timezone
+dpkg-reconfigure tzdata
+
+
+# Basic config:
+# Enable 1Wire en I2C during boot
+if [ -f /boot/config.txt ]; then
+
+  if [ `grep -ic "#dtparam=i2c_arm=on" /boot/config.txt` -eq 1 ]; then
+    sed -i.bak 's/^#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/config.txt
+  fi
+  if [ `grep -ic "dtparam=i2c_arm=on" /boot/config.txt` -eq 0 ]; then
+    echo "dtparam=i2c_arm=on" >> /boot/config.txt
+  fi
+
+  if [ `grep -ic "dtoverlay=w1-gpio" /boot/config.txt` -eq 0 ]; then
+    echo "dtoverlay=w1-gpio" >> /boot/config.txt
+  fi
+
+  # Enable camera
+  if [ `grep -ic "gpu_mem=" /boot/config.txt` -eq 0 ]; then
+    echo "gpu_mem=128" >> /boot/config.txt
+  fi
+
+fi
+
+# Create needed groups
+groupadd -f dialout 2> /dev/null
+groupadd -f sispmctl 2> /dev/null
+groupadd -f gpio 2> /dev/null
+# Add user to all groupds
+usermod -a -G dialout,sispmctl,gpio ${SCRIPT_USER} 2> /dev/null
+
+
+# Docu https://pylibftdi.readthedocs.io/
+# Make sure that the normal Pi user can read and write to the usb driver
+echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", GROUP="dialout", MODE="0660"' > /etc/udev/rules.d/99-libftdi.rules
+echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", GROUP="dialout", MODE="0660"' >> /etc/udev/rules.d/99-libftdi.rules
+
+# https://pypi.python.org/pypi/pysispm
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd10", GROUP="sispmctl", MODE="660"' > /etc/udev/rules.d/60-sispmctl.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd11", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd12", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd13", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd15", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
+
+# Reload udev controll
+udevadm control --reload-rules
+
+# Install 1 Wire I2C stuff
+if [ -f /etc/owfs.conf ]; then
+  sed -i.bak 's/^server: FAKE = DS18S20,DS2405/#server: FAKE = DS18S20,DS2405/' /etc/owfs.conf
+
+  if [ `grep -ic "server: device=/dev/i2c-1" /etc/owfs.conf` -eq 0 ]; then
+    echo "server: device=/dev/i2c-1" >> /etc/owfs.conf
+  fi
+fi
+
+if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
+  sed -i.bak 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+fi
+
+if [ -f /etc/modules ]; then
+  if [ `grep -ic "i2c-dev" /etc/modules` -eq 0 ]; then
+    echo "i2c-dev" >> /etc/modules
+  fi
+fi
+
+# Make sure pigpiod is started at boot, and that user PI can restart it with sudo command
+echo "${SCRIPT_USER} ALL=(ALL) NOPASSWD: /usr/sbin/service pigpiod restart" > /etc/sudoers.d/terrariumpi
+systemctl enable pigpiod
+
+# Remove unneeded OWS services
+update-rc.d -f owftpd remove
+update-rc.d -f owfhttpd remove
+
 PROGRESS=35
 # Update submodules if downloaded through tar or zip
 (
@@ -147,7 +223,6 @@ if [ $PYTHON -eq 3 ]; then
   pip3 uninstall -y -q numpy
 fi
 
-
 cd "${BASEDIR}"
 chown ${SCRIPT_USER}. .
 chown ${SCRIPT_USER}. * -Rf
@@ -163,73 +238,6 @@ EOF
 sleep 1
 ) | whiptail --backtitle "${INSTALLER_TITLE}" --title " TerrariumPI Installer " --gauge "Install required software\n\nInstalling python module ${PIP_MODULE} ..." 0 78 0
 
-# Basic config:
-# Enable 1Wire en I2C during boot
-if [ -f /boot/config.txt ]; then
-
-  if [ `grep -ic "#dtparam=i2c_arm=on" /boot/config.txt` -eq 1 ]; then
-    sed -i.bak 's/^#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/config.txt
-  fi
-  if [ `grep -ic "dtparam=i2c_arm=on" /boot/config.txt` -eq 0 ]; then
-    echo "dtparam=i2c_arm=on" >> /boot/config.txt
-  fi
-
-  if [ `grep -ic "dtoverlay=w1-gpio" /boot/config.txt` -eq 0 ]; then
-    echo "dtoverlay=w1-gpio" >> /boot/config.txt
-  fi
-
-  # Enable camera
-  if [ `grep -ic "gpu_mem=" /boot/config.txt` -eq 0 ]; then
-    echo "gpu_mem=128" >> /boot/config.txt
-  fi
-
-fi
-
-# Create needed groups
-groupadd -f dialout 2> /dev/null
-groupadd -f sispmctl 2> /dev/null
-groupadd -f gpio 2> /dev/null
-# Add user to all groupds
-usermod -a -G dialout,sispmctl,gpio ${SCRIPT_USER} 2> /dev/null
-
-
-# Docu https://pylibftdi.readthedocs.io/
-# Make sure that the normal Pi user can read and write to the usb driver
-echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", GROUP="dialout", MODE="0660"' > /etc/udev/rules.d/99-libftdi.rules
-echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", GROUP="dialout", MODE="0660"' >> /etc/udev/rules.d/99-libftdi.rules
-
-# https://pypi.python.org/pypi/pysispm
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd10", GROUP="sispmctl", MODE="660"' > /etc/udev/rules.d/60-sispmctl.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd11", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd12", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd13", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="fd15", GROUP="sispmctl", MODE="660"' >> /etc/udev/rules.d/60-sispmctl.rules
-
-# Reload udev controll
-udevadm control --reload-rules
-
-# Install 1 Wire I2C stuff
-if [ -f /etc/owfs.conf ]; then
-  sed -i.bak 's/^server: FAKE = DS18S20,DS2405/#server: FAKE = DS18S20,DS2405/' /etc/owfs.conf
-
-  if [ `grep -ic "server: device=/dev/i2c-1" /etc/owfs.conf` -eq 0 ]; then
-    echo "server: device=/dev/i2c-1" >> /etc/owfs.conf
-  fi
-fi
-
-if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
-  sed -i.bak 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
-fi
-
-if [ -f /etc/modules ]; then
-  if [ `grep -ic "i2c-dev" /etc/modules` -eq 0 ]; then
-    echo "i2c-dev" >> /etc/modules
-  fi
-fi
-
-# Make sure pigpiod is started at boot, and that user PI can restart it with sudo command
-echo "${SCRIPT_USER} ALL=(ALL) NOPASSWD: /usr/sbin/service pigpiod restart" > /etc/sudoers.d/terrariumpi
-systemctl enable pigpiod
 
 # To run this as non-root run the following, https://github.com/marcelrv/miflora, https://github.com/IanHarvey/bluepy/issues/218
 if [ $PYTHON -eq 2 ]; then
@@ -237,11 +245,6 @@ if [ $PYTHON -eq 2 ]; then
 elif [ $PYTHON -eq 3 ]; then
   setcap 'cap_net_raw,cap_net_admin+eip' /usr/local/lib/python3.5/dist-packages/bluepy/bluepy-helper
 fi
-
-
-# Remove unneeded OWS services
-update-rc.d -f owftpd remove
-update-rc.d -f owfhttpd remove
 
 # Move log file to temprorary mount
 if grep -qs "${TMPFS} " /proc/mounts; then
@@ -269,9 +272,6 @@ fi
 if [ `grep -ic "start.sh" /etc/rc.local` -eq 0 ]; then
   sed -i.bak "s@^exit 0@# Starting TerrariumPI server\n${BASEDIR}/start.sh\n\nexit 0@" /etc/rc.local
 fi
-
-# Set the timezone
-dpkg-reconfigure tzdata
 
 # We are done!
 sync
