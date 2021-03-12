d40a1180499286c1e1befd670fdd8007c622c44b
TheYOSH <github.com@theyosh.nl>
2021-03-12 17:25:59 +0100

    updates

--- terrariumPI.py
+++ terrariumPI.py
@@ -7,9 +7,6 @@ monkey.patch_all()
 import gettext
 gettext.install('terrariumpi', 'locales/')
 
-import warnings
-#warnings.simplefilter("ignore", ResourceWarning)
-
 import terrariumLogging
 logger = terrariumLogging.logging.getLogger(__name__)
 
@@ -19,10 +16,7 @@ GPIO.setmode(GPIO.BCM)
 from terrariumEngine import terrariumEngine
 
 if __name__  == "__main__":
-  with warnings.catch_warnings():
-    warnings.simplefilter("ignore", ResourceWarning)
-    terrariumEngine = terrariumEngine(__version__)
-    # This keeps running until CRTL-C is entered or given
-    terrariumEngine.stop()
-
+  terrariumEngine = terrariumEngine(__version__)
+  # This keeps running until CRTL-C is entered or given
+  terrariumEngine.stop()
   logger.info(f'Shutdown TerrariumPI {__version__} done. Bye bye ...')
