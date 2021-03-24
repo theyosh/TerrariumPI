6c0bf8d35a2f758ce059ffe1f0aca3dec60fd4a4
TheYOSH <github.com@theyosh.nl>
2021-03-24 14:20:02 +0100

    Add support for BH1750 LUX light sensor (untested)

---
+++ hardware/sensor/bh1750_sensor.py
@@ -0,0 +1,33 @@
+from . import terrariumI2CSensor, terrariumSensorException
+from terrariumUtils import terrariumUtils
+
+# https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/python/bh1750.py
+
+class terrariumBH1750Sensor(terrariumI2CSensor):
+  HARDWARE = 'bh1750'
+  TYPES    = ['light',]
+  NAME     = 'BH1750 LUX light sensor'
+
+  # Start measurement at 4lx resolution. Time typically 16ms.
+  CONTINUOUS_LOW_RES_MODE = 0x13
+  # Start measurement at 1lx resolution. Time typically 120ms
+  CONTINUOUS_HIGH_RES_MODE_1 = 0x10
+  # Start measurement at 0.5lx resolution. Time typically 120ms
+  CONTINUOUS_HIGH_RES_MODE_2 = 0x11
+  # Start measurement at 1lx resolution. Time typically 120ms
+  # Device is automatically set to Power Down after measurement.
+  ONE_TIME_HIGH_RES_MODE_1 = 0x20
+  # Start measurement at 0.5lx resolution. Time typically 120ms
+  # Device is automatically set to Power Down after measurement.
+  ONE_TIME_HIGH_RES_MODE_2 = 0x21
+  # Start measurement at 1lx resolution. Time typically 120ms
+  # Device is automatically set to Power Down after measurement.
+  ONE_TIME_LOW_RES_MODE = 0x23
+
+  def _get_data(self):
+    data = None
+    with self._open_hardware() as i2c_bus:
+      data = i2c_bus.read_i2c_block_data(self.device[0],terrariumBH1750Sensor.ONE_TIME_HIGH_RES_MODE_1)
+      data['light'] = (data[1] + (256 * data[0])) / 1.2
+
+    return data
