10b6ea465a39d64c3c65c521f455e28363e18715
TheYOSH <github.com@theyosh.nl>
2021-05-05 16:04:32 +0200

    Doc updates

--- docs/tools/test.sh
+++ docs/tools/test.sh
@@ -56,7 +56,7 @@ while (($#)); do
 done
 
 if $_build; then
-  JEKYLL_ENV=production bundle exec jekyll b
+  JEKYLL_ENV=production bundle exec jekyll b -s docs
 fi
 
 bundle exec htmlproofer "$DEST" \
