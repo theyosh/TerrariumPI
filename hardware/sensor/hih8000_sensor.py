db2d7156b76a122da4935afcaf59d550b8efc82e
TheYOSH <github.com@theyosh.nl>
2021-05-13 22:14:36 +0200

    Add support for Honeywell HumidIcon HIH8000

---
+++ hardware/sensor/hih8000_sensor.py
@@ -0,0 +1,23 @@
+from . import terrariumI2CSensor
+from terrariumUtils import terrariumUtils
+
+from gevent import sleep
+
+class terrariumHIH8000Sensor(terrariumI2CSensor):
+  HARDWARE = 'hih8000'
+  TYPES    = ['temperature','humidity']
+  NAME     = 'Honeywell HumidIcon HIH8000'
+
+  def _get_data(self):
+    data = None
+
+    with self._open_hardware() as i2c_bus:
+      i2c_bus.write_quick(self.device[0])
+      sleep(0.04)
+      raw_data = i2c_bus.read_i2c_block_data(self.device[0], 0, 4)
+
+      data = {}
+      data['humidity']    = ((raw_data[0] & 0x3f) * 256 + raw_data[1]) / float(0x3ffe) * 100
+      data['temperature'] = ((raw_data[2] * 256 + raw_data[3]) >> 2) / float(0x3ffe) * 165 - 40
+
+    return data
