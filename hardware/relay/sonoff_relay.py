e70ec178da92ead42a9dd0bac4f7c08816cb91dc
TheYOSH <github.com@theyosh.nl>
2021-07-14 10:59:35 +0200

    Fix for now for ZeroConf

--- hardware/relay/sonoff_relay.py
+++ hardware/relay/sonoff_relay.py
@@ -11,6 +11,23 @@ import re
 # https://python-zeroconf.readthedocs.io/
 # Needs mDNS: https://tasmota.github.io/docs/Commands/#setoption55 => SO55 1
 
+
+import functools
+def force_sync(fn):
+    '''
+    turn an async function to sync function
+    '''
+    import asyncio
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        res = fn(*args, **kwargs)
+        if asyncio.iscoroutine(res):
+            return asyncio.get_event_loop().run_until_complete(res)
+        return res
+
+    return wrapper
+
 class terrariumRelaySonoff(terrariumRelay):
   HARDWARE = 'sonoff'
   NAME = 'Sonoff (Tasmota)'
