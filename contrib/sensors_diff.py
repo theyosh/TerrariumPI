1058efbce73c4c2ba0934a2666f4e12370d31e35
TheYOSH <github.com@theyosh.nl>
2021-07-19 17:47:38 +0200

    Update for TP4

--- contrib/sensors_diff.py
+++ contrib/sensors_diff.py
@@ -2,7 +2,7 @@
 import requests
 
 # Enter here the two sensors to measure the difference
-# You can find the ID values in the settings.cfg file after you have added the individual sensors.
+# Get a full list of sensors data at: http://localhost:8090/api/sensors/ and find the sensor you need. Copy the value of the field 'id'
 SENSORS=['dcd784579ab801750421dd3634d69fd5','176ba2cc660046a239e15b1f94ef8091']
 # Enter the hostname for TerrariumPI. Should be working out of the box
 TERRARIUMPI_SERVER='http://localhost:8090'
@@ -13,7 +13,7 @@ if len(SENSORS) == 2:
   try:
     sensor1_value = requests.get('{}/api/sensors/{}'.format(TERRARIUMPI_SERVER,SENSORS[0]))
     if sensor1_value.status_code == 200:
-      sensor1_value = sensor1_value.json()['sensors'][0]['current']
+      sensor1_value = sensor1_value.json()['current']
 
   except Exception as ex:
     sensor1_value = None
@@ -22,7 +22,7 @@ if len(SENSORS) == 2:
     try:
       sensor2_value = requests.get('{}/api/sensors/{}'.format(TERRARIUMPI_SERVER,SENSORS[1]))
       if sensor2_value.status_code == 200:
-        sensor2_value = sensor2_value.json()['sensors'][0]['current']
+        sensor2_value = sensor2_value.json()['current']
         difference = abs(sensor1_value - sensor2_value)
 
     except Exception as ex:
