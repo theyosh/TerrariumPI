27d262b96a2f34bc6e7e9918b2773ad01275503b
TheYOSH <github.com@theyosh.nl>
2021-08-09 19:30:42 +0200

    Support for Sonoff D1 dimmer. #579

--- hardware/relay/sonoff_relay.py
+++ hardware/relay/sonoff_relay.py
@@ -1,4 +1,4 @@
-from . import terrariumRelay, terrariumRelayLoadingException
+from . import terrariumRelay, terrariumRelayDimmer, terrariumRelayLoadingException
 from terrariumUtils import terrariumUtils
 
 from zeroconf import ServiceBrowser, Zeroconf
@@ -11,29 +11,12 @@ import re
 # https://python-zeroconf.readthedocs.io/
 # Needs mDNS: https://tasmota.github.io/docs/Commands/#setoption55 => SO55 1
 
-
-import functools
-def force_sync(fn):
-    '''
-    turn an async function to sync function
-    '''
-    import asyncio
-
-    @functools.wraps(fn)
-    def wrapper(*args, **kwargs):
-        res = fn(*args, **kwargs)
-        if asyncio.iscoroutine(res):
-            return asyncio.get_event_loop().run_until_complete(res)
-        return res
-
-    return wrapper
-
 class terrariumRelaySonoff(terrariumRelay):
   HARDWARE = 'sonoff'
   NAME = 'Sonoff (Tasmota)'
 
   __URL_REGEX = re.compile(r'^(?P<protocol>https?):\/\/((?P<user>[^:]+):(?P<passwd>[^@]+)@)?(?P<host>[^#\/]+)(\/)?(#(?P<nr>\d+))?$',re.IGNORECASE)
-  __SCAN_TIME = 5
+#  __SCAN_TIME = 5
 
   @property
   def _address(self):
@@ -110,35 +93,79 @@ class terrariumRelaySonoff(terrariumRelay):
 
     return self.ON if terrariumUtils.is_true(data) else self.OFF
 
-  @staticmethod
-  def _scan_relays(callback=None, **kwargs):
-    found_devices = []
+class terrariumRelayDimmerSonoffD1(terrariumRelayDimmer):
+  HARDWARE = 'sonoff_d1-dimmer'
+  NAME = 'Sonoff D1 Dimmer (Tasmota)'
 
-    class Listener():
-      def remove_service(self, zeroconf, type, name):
-        pass
+  __URL_REGEX = re.compile(r'^(?P<protocol>https?):\/\/((?P<user>[^:]+):(?P<passwd>[^@]+)@)?(?P<host>[^#\/]+)(\/)?(#(?P<nr>\d+))?$',re.IGNORECASE)
 
-      def add_service(self, zeroconf, type, name):
-        info = zeroconf.get_service_info(type, name)
-        # Check for Tasmota firmware...
-        address = f'{info.parsed_addresses()[0]}:{info.port}'
-        data = terrariumUtils.get_remote_data(f'http://{address}/cm?cmnd=Status')
-        if data is not None and 'Status' in data and 'FriendlyName' in data['Status']:
-          for switch_nr in range(len(data['Status']['FriendlyName'])):
-            found_devices.append(terrariumRelay(None,
-                                                terrariumRelaySonoff.HARDWARE,
-                                                f'http://{address}#{switch_nr+1}',
-                                                f'{terrariumRelaySonoff.NAME} {data["Status"]["FriendlyName"][switch_nr]}({switch_nr+1}) device ip: {address}',
-                                                callback=callback) )
+  @property
+  def _address(self):
+    address = None
+    data = self.__URL_REGEX.match(self.address)
+    if data:
+      address = data.groupdict()
+      if 'nr' not in address or address['nr'] is None or '' == address['nr']:
+        address['nr'] = 1
 
-    zeroconf = Zeroconf()
-    try:
-      browser = ServiceBrowser(zeroconf, "_http._tcp.local.", Listener())
-      sleep(terrariumRelaySonoff.__SCAN_TIME)
-    except OSError:
-      pass
-    finally:
-      zeroconf.close()
+      address['nr'] = int(address['nr'])
+    else:
+      raise terrariumRelayLoadingException(f'Incorrect address for a Sonoff Dimmer device: {self}')
 
-    for device in found_devices:
-       yield device
+    return address
+
+  # This will update the device based in mac address
+  def load_hardware(self):
+    super().load_hardware()
+    # Bad choice: But reload the hardware once more to get the right mac address for unique ID. aKa forcing reloading, ignoring the hardware cache
+    self._load_hardware()
+
+  def _load_hardware(self):
+    # Input format should be either:
+    # - http://[HOST]#[POWER_SWITCH_NR]
+    # - http://[HOST]/#[POWER_SWITCH_NR]
+    # - http://[PASSWORD]@[HOST]#[POWER_SWITCH_NR]
+    # - http://[PASSWORD]@[HOST]/#[POWER_SWITCH_NR]
+
+    address = self._address
+
+    # Try Tasmota
+    # http://sonoff/cm?cmnd=Power[POWER_SWITCH_NR]%201
+    # http://sonoff/cm?cmnd=Power[POWER_SWITCH_NR]%200
+    # http://sonoff/cm?user=admin&password=joker&cmnd=Power[POWER_SWITCH_NR]%201
+
+    device = f'{address["protocol"]}://{address["host"]}/cm?'
+
+    if 'user' in address and 'password' in address:
+      device += f'user={address["user"]}&password={address["password"]}&'
+
+    device += 'cmnd='
+    state = terrariumUtils.get_remote_data(f'{device}Status%200')
+
+    if state is None:
+      return None
+
+    # Always overule the ID generating, as we want to use the MAC as that is unique if the IP address is changing
+    self.id = md5(f'{self.HARDWARE}{state["StatusNET"]["Mac"].lower()}'.encode()).hexdigest()
+
+    return device
+
+  def _set_hardware_value(self, state):
+    state = int(max(0.0,min(100.0,float(state + self._dimmer_offset))))
+
+    url = f'{self.device}Dimmer%20{state}'
+    data = terrariumUtils.get_remote_data(url)
+
+    if data is None:
+      return False
+
+    return state == int(data['Dimmer'])
+
+  def _get_hardware_value(self):
+    url = f'{self.device}Dimmer'
+    data = terrariumUtils.get_remote_data(url)
+
+    if data is not None and 'Dimmer' in data:
+      return int(data['Dimmer'])
+
+    return None
