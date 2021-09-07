f7ed3c62a2df74e7260f7136c1fe841bcbc8deeb
TheYOSH <github.com@theyosh.nl>
2021-09-07 18:00:51 +0200

    Fixed stupid bug. And made a better test example #603

--- hardware/relay/script_relay.py
+++ hardware/relay/script_relay.py
@@ -22,18 +22,17 @@ class terrariumRelayScript(terrariumRelay):
     cmd = f'{self.address} {state}'
 
     try:
-      data = terrariumUtils.get_script_data(cmd).decode('utf-8').strip()
+      terrariumUtils.get_script_data(cmd)
     except subprocess.CalledProcessError as ex:
       # Device does not exists....
       return None
 
-    return self.ON if state != 0.0 else self.OFF
+    return True
 
   def _get_hardware_value(self):
 
     try:
-      data = terrariumUtils.get_script_data(self.address).decode('utf-8').strip()
-      data = float(data)
+      data = float(terrariumUtils.get_script_data(self.address).decode('utf-8').strip())
 
       # When the return value is -1, it means that there is not readout possible. So return the current state from memory
       if -1.0 == data:
@@ -53,7 +52,7 @@ class terrariumDimmerScript(terrariumRelayDimmer):
     if not script.exists():
       raise terrariumRelayLoadingException(f'Invalid script location for relay {self}: {self.address}')
 
-    if oct(script.stat().st_mode)[-3:] not in ['777','775','755']:
+    if oct(script.stat().st_mode)[-3:] not in ['777','775','755','744','544','554','555','550','540','770','750','740']:
       raise terrariumRelayLoadingException(f'Script {self.address} for relay {self} is not executable.')
 
     return self.address
