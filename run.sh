3185832c50f9b582cec93bc43264348fda0e20f6
Alec <alec.pinson@fedex.com>
2021-11-14 08:55:28 +0000

    fix pi camera also include openjdk in the docker image

--- run.sh
+++ run.sh
@@ -81,6 +81,9 @@ if [ ! -h log/terrariumpi.access.log ]; then
   ln -s /dev/shm/terrariumpi.access.log log/terrariumpi.access.log
 fi
 
+# Add some pi camera symbolic links
+ln -s /opt/vc/bin/raspi* /usr/bin/ 2>/dev/null
+
 if [[ $REBOOT_REQUIRED == 1 ]]; then
   if [[ $AUTO_REBOOT == true ]]; then
     echo "Some settings have been updated that require reboot, your Raspberry Pi will reboot in 60 seconds..."
