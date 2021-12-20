1b7e5d2422f0a8480bac045bfb1a504633b0f1a6
Charles Nelson <cnelsonsic@gmail.com>
2021-12-19 17:45:03 -0800

    Final polish for lywsd03mmc

--- Makefile
+++ Makefile
@@ -1,10 +1,20 @@
 IMAGE=cnelson/terrariumpi
 VERSION=4.1.0
 
-.PHONY: all terrariumpi
+.PHONY: all build run logs
 
-all: terrariumpi
+all: build
 
-terrariumpi:
-	sudo docker buildx build --platform linux/arm/v7 -t $(IMAGE):$(VERSION) -f Dockerfile .
+build:
+	sudo docker buildx build \
+		--progress=plain \
+		--platform linux/arm/v7 \
+		-t $(IMAGE):$(VERSION) \
+		-f Dockerfile \
+		.
 
+run: build
+	cd /media/external/runtime; sudo docker-compose down; sudo docker-compose up -d
+
+logs:
+	cd /media/external/runtime; sudo docker-compose logs -f
--- hardware/sensor/lywsd03mmc_sensor.py
+++ hardware/sensor/lywsd03mmc_sensor.py
@@ -1,32 +1,40 @@
+import terrariumLogging
+logger = terrariumLogging.logging.getLogger(__name__)
+
 from . import terrariumBluetoothSensor
 
 # pip install lywsd03mmc
 from lywsd03mmc import Lywsd03mmcClient
+from bluepy.btle import BTLEDisconnectError, BTLEInternalError, BTLEGattError, BTLEException
 
 class terrariumLywsd03mmcSensor(terrariumBluetoothSensor):
   HARDWARE = 'LYWSD03MMC'
-  TYPES    = ['temperature','humidity']
+  TYPES    = ['temperature','humidity'] # Also supports battery, but unused.
   NAME     = 'LYWSD03MMC bluetooth sensor'
+
+  _UPDATE_TIME_OUT = 30 # This has to be >~25 to reliably get data.
+  #_CACHE_TIMEOUT = 60 # Set this higher to avoid errors.
 
   def _load_hardware(self):
     address = self._address
+    logger.debug("Started connecting")
     device = Lywsd03mmcClient(address[0])
+    logger.debug("Finished connecting")
     return device
 
   def _get_data(self):
     data = {}
 
-    # BUG: Firmware data en battery will only be set at the first measurement.
-    #self._device['firmware'] = self._device['device'].firmware_version()
-    #self._device['battery']  = self._device['device'].parameter_value(MI_BATTERY)
+    logger.debug("starting to fetch data")
 
-    data['temperature'] = self.device.temperature
-    data['humidity']    = self.device.humidity
-    data['battery']     = self.device.battery
+    for sensor_type in self.TYPES:
+        data[sensor_type] = getattr(self.device, sensor_type)
+
+    logger.debug(data)
 
     return data
 
   @staticmethod
   def _scan_sensors(unit_value_callback = None, trigger_callback = None):
-    for sensor in terrariumBluetoothSensor._scan_sensors(__class__, ['mj_ht_v1'], unit_value_callback, trigger_callback):
+    for sensor in terrariumBluetoothSensor._scan_sensors(__class__, [__class__.HARDWARE], unit_value_callback, trigger_callback):
       yield sensor
