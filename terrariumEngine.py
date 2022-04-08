ff1ba3102b48df60dfb22dbbc647d376dfe7bb59
TheYOSH <github.com@theyosh.nl>
2022-04-08 14:42:50 +0200

    Do not show commit version when nog available

--- terrariumEngine.py
+++ terrariumEngine.py
@@ -300,16 +300,17 @@ class terrariumEngine(object):
     # Loading active language
     gettext.translation('terrariumpi', 'locales/', languages=[self.settings['language']]).install()
 
-    # TODO: Loading git data
+    # Loading git data
     gitversion = Path('.gitversion')
     if gitversion.exists():
       gitversion = gitversion.read_text().strip()
-    elif Path('.git').exists():
+      gitversion = None if gitversion == '' else gitversion
+
+    if gitversion is None and Path('.git').exists():
       gitversion = str(terrariumUtils.get_script_data('git rev-parse HEAD')).strip()
     else:
       gitversion = None
 
-    print(f'Setting git version to: {gitversion}')
     self.settings['gitversion'] = gitversion
 
     # Loading weather
