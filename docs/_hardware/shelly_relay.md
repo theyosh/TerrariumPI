23dbc5527e46f5b912256f546913bb0be5a0094c
TheYOSH <github.com@theyosh.nl>
2023-01-05 22:26:29 +0100

    Clear debug and add documentation. #769

---
+++ docs/_hardware/shelly_relay.md
@@ -0,0 +1,28 @@
+---
+title: Shelly
+categories: [Hardware, Relay]
+tags: [relay, wifi, shelly]
+
+image:
+  path: /assets/img/Shelly.webp
+  src: /assets/img/Shelly.webp
+  alt: "Shelly header image"
+
+device_address: "Enter the hostname or IP address and optional a relay number separated by a dash.<br />Ex: `http://[PASSWORD]@[HOST]#[POWER_SWITCH_NR]`"
+device_url : https://www.shelly.cloud/en-de/products/switching-and-triggering
+---
+
+## Information
+
+WiFi-operated smart relay, 1 channel 16A
+
+Control a wide range of home appliances and office equipment (lights, power lines, security systems, heaters, radiators, sockets, LED strips, etc.) from anywhere.
+
+- Dry contacts
+- Extremely fast processor – ESP32, Improved API interface
+- Enhanced safety
+- Enhanced security - MQTT and WSS support, TLS and custom certificates support
+- Designed to fit in most standard electrical boxes and switches
+- Easy control through the Shelly app, various protocols, and platforms, as well as voice assistants
+
+{% include_relative _relay_detail.md %}
--- hardware/relay/shelly_relay.py
+++ hardware/relay/shelly_relay.py
@@ -25,12 +25,6 @@ class terrariumRelayShelly(terrariumRelay):
 
     return address
 
-  # This will update the device based in mac address
-  #def load_hardware(self):
-  #  super().load_hardware()
-  #  # Bad choice: But reload the hardware once more to get the right mac address for unique ID. aKa forcing reloading, ignoring the hardware cache
-  #  self._load_hardware()
-
   def _load_hardware(self):
     # Input format should be either:
     # - http://[HOST]#[POWER_SWITCH_NR]
@@ -44,34 +38,18 @@ class terrariumRelayShelly(terrariumRelay):
     if 'user' in address and 'password' in address:
       device = f'{address["protocol"]}://{address["user"]}:{address["password"]}@{address["host"]}'
 
-
     device += f'/relay/{self._address["nr"]}'
-
-    print('Device')
-    print(device)
 
     data = terrariumUtils.get_remote_data(device)
 
     if data is None:
       return None
 
-    # Create the cache key for caching the relay states.
-    # This is usefully when there are more then 1 relay per hardware device.
-    # self.__cache_key = md5(f'{self.HARDWARE}{address["host"].lower()}'.encode()).hexdigest()
-    # self.__cache = terrariumCache()
-    # self.__cache.set_data(self.__cache_key, state['relays'], self._CACHE_TIMEOUT)
-
-    # We need the use the address_nr value also, as there can multiple relays per sonoff device.
-#    if self._device['id'] is None:
-#      self.id = md5(f'{self.HARDWARE}{state["StatusNET"]["Mac"].lower()}{address["nr"]}'.encode()).hexdigest()
-
-
     return device
 
   def _set_hardware_value(self, state):
     action = 'on' if state == self.ON else 'off'
     url = f'{self.device}?turn={action}'
-    print(f'Set hardware url: {url}')
     data = terrariumUtils.get_remote_data(url)
 
     if data is None:
@@ -83,20 +61,6 @@ class terrariumRelayShelly(terrariumRelay):
     return state == (self.ON if terrariumUtils.is_true(data) else self.OFF)
 
   def _get_hardware_value(self):
-
-    # data = self.__cache.get_data(self.__cache_key)
-    # if data is None:
-    #   # Cache is expired, so we update with new data
-    #   # Get the overall state information
-    #   url = f'{self.device}status'
-    #   print(f'Get hardware url: {url}')
-    #   data = terrariumUtils.get_remote_data(url)
-
-    #   if data is None:
-    #     return None
-
-    #   self.__cache.set_data(self.__cache_key, data['relays'], self._CACHE_TIMEOUT)
-
     data = terrariumUtils.get_remote_data(self.device)
 
     if data is None:
