da2c0287765a9da599d35f6af3f530d74ac86ee7
TheYOSH <github.com@theyosh.nl>
2023-04-10 13:34:18 +0200

    Fix or better read out AM2320 sensor. #794

--- hardware/sensor/am2320_sensor.py
+++ hardware/sensor/am2320_sensor.py
@@ -4,8 +4,91 @@ logger = terrariumLogging.logging.getLogger(__name__)
 
 from . import terrariumI2CSensor
 
-from struct import unpack
+# Source: http://www.pibits.net/code/am2320-temperature-and-humidity-sensor-and-raspberry-pi-example.php#codesyntax_1
+# Slightly modified by TheYOSH
+import posix
+from fcntl import ioctl
 from time import sleep
+
+class AM2320:
+  I2C_ADDR = 0x5c
+  I2C_SLAVE = 0x0703
+
+  def __init__(self, i2cbus = 1, address = I2C_ADDR):
+    self._i2cbus = i2cbus
+    self._address = address
+
+  @staticmethod
+  def _calc_crc16(data):
+    crc = 0xFFFF
+
+    for x in data:
+      crc = crc ^ x
+      for bit in range(0, 8):
+        if (crc & 0x0001) == 0x0001:
+          crc >>= 1
+          crc ^= 0xA001
+        else:
+          crc >>= 1
+
+    return crc
+
+  @staticmethod
+  def _combine_bytes(msb, lsb):
+    return msb << 8 | lsb
+
+  def readSensor(self):
+    fd = posix.open("/dev/i2c-%d" % self._i2cbus, posix.O_RDWR)
+    ioctl(fd, self.I2C_SLAVE, self._address)
+    # wake AM2320 up, goes to sleep to not warm up and affect the humidity sensor
+    # This write will fail as AM2320 won't ACK this write
+    try:
+      posix.write(fd, b'\0x00')
+    except:
+      pass
+
+    sleep(0.002) #Wait at least 0.8ms, at most 3ms
+    # write at addr 0x03, start reg = 0x00, num regs = 0x04 */
+    posix.write(fd, b'\x03\x00\x04')
+    sleep(0.0020) #Wait at least 1.5ms for result
+
+    # Read out 8 bytes of result data
+    # Byte 0: Should be Modbus function code 0x03
+    # Byte 1: Should be number of registers to read (0x04)
+    # Byte 2: Humidity msb
+    # Byte 3: Humidity lsb
+    # Byte 4: Temperature msb
+    # Byte 5: Temperature lsb
+    # Byte 6: CRC lsb byte
+    # Byte 7: CRC msb byte
+    data = bytearray(posix.read(fd, 8))
+
+    # Check data[0] and data[1]
+    if data[0] != 0x03 or data[1] != 0x04:
+      raise Exception("First two read bytes are a mismatch")
+
+    # CRC check
+    if self._calc_crc16(data[0:6]) != self._combine_bytes(data[7], data[6]):
+      raise Exception("CRC failed")
+
+    # Temperature resolution is 16Bit,
+    # temperature highest bit (Bit15) is equal to 1 indicates a
+    # negative temperature, the temperature highest bit (Bit15)
+    # is equal to 0 indicates a positive temperature;
+    # temperature in addition to the most significant bit (Bit14 ~ Bit0)
+    # indicates the temperature sensor string value.
+    # Temperature sensor value is a string of 10 times the
+    # actual temperature value.
+    temp = self._combine_bytes(data[4], data[5])
+    if temp & 0x8000:
+      temp = -(temp & 0x7FFF)
+      temp /= 10.0
+    else:
+      temp /= 10.0
+
+    humi = self._combine_bytes(data[2], data[3]) / 10.0
+    return (temp, humi)
+
 
 class terrariumAM2320Sensor(terrariumI2CSensor):
 
@@ -13,49 +96,14 @@ class terrariumAM2320Sensor(terrariumI2CSensor):
   TYPES    = ['temperature','humidity']
   NAME     = 'AM2320 sensor'
 
-  __PARAM_AM2320_READ = 0x03
-  __REG_AM2320_HUMIDITY_MSB = 0x00
-
-  def __get_raw_data(self,command, regaddr, regcount):
-
-    def __am_crc16(buf):
-      crc = 0xFFFF
-      for c in buf:
-        crc ^= c
-        for _ in range(8):
-          if crc & 0x01:
-            crc >>= 1
-            crc ^= 0xA001
-          else:
-            crc >>= 1
-      return crc
-
-    with self._open_hardware() as i2c_bus:
-      try:
-        # wake AM2320 up, goes to sleep to not warm up and affect the humidity sensor
-        # This write will fail as AM2320 won't ACK this write
-        i2c_bus.write_i2c_block_data(self.device[0], 0x00, [])
-      except Exception as ex:
-        logger.debug(f'Wake up {self} hardware: {ex}')
-
-      sleep(0.01)
-      i2c_bus.write_i2c_block_data(self.device[0], command, [regaddr, regcount])
-      sleep(0.002)
-      buf = i2c_bus.read_i2c_block_data(self.device[0], 0, 8)
-
-    buf_str = ''.join(chr(x) for x in buf)
-
-    crc = unpack('<H', buf_str[-2:])[0]
-    if crc != __am_crc16(buf[:-2]):
-      logger.error('AM2320 CRC error for sensor {}'.format(self.get_name()))
-      return None
-
-    return buf_str[2:-2]
+  def _load_hardware(self):
+    address = self._address
+    device = AM2320(address[1],address[0])
+    return device
 
   def _get_data(self):
     data = {}
-    sensor_data = self.__get_raw_data(self.__PARAM_AM2320_READ, self.__REG_AM2320_HUMIDITY_MSB, 4)
-    data['temperature'] = unpack('>H', sensor_data[-2:])[0] / 10.0
-    data['humidity']    = unpack('>H', sensor_data[-4:2])[0] / 10.0
-
+    values = self.device.readSensor()
+    data['temperature'] = values[0]
+    data['humidity']    = values[1]
     return data
