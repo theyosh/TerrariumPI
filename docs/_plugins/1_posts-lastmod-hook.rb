fab22a8a222b7d0957749efa4336d7946e9c903c
TheYOSH <github.com@theyosh.nl>
2023-05-14 12:40:38 +0200

    Plugin update

--- docs/_plugins/1_posts-lastmod-hook.rb
+++ docs/_plugins/1_posts-lastmod-hook.rb
@@ -5,6 +5,10 @@ Jekyll::Hooks.register :documents, :post_init do |post|
   commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
   if commit_num.to_i > 1
+    # Somehow the post date is always the latest commit... :(
+    post_date = `git log --reverse --pretty="%ad" --date=iso "#{ post.path }" | head -1`
+    post.data['date'] = post_date
+
     lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
     post.data['last_modified_at'] = lastmod_date
   end
