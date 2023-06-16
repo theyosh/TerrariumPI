51899026c6ae67b39312ce33ce0cae299b2cdd44
TheYOSH <github.com@theyosh.nl>
2023-06-16 12:49:38 +0200

    Fix warning icons

--- gui/user-controls/EnclosureCard.svelte
+++ gui/user-controls/EnclosureCard.svelte
@@ -118,9 +118,9 @@
                     {settings.units[area.type].value},
                     {$_('enclosures.area.now', { default: 'now' })}: {roundToPrecision(area.state.sensors.current)}
                     {settings.units[area.type].value}
-                    <i class="fas fa-exclamation-triangle text-muted mt-1 ml-2"
-                        class:text-danger="{(area.state.sensor?.alarm_low  && area.setup.low.relays.length  > 0)
-                                         || (area.state.sensor?.alarm_high && area.setup.high.relays.length > 0)}"
+                    <i class="fas fa-exclamation-triangle text-secondary mt-1 ml-2"
+                        class:text-danger="{(area.state.sensors?.alarm_low  && area.setup.low.relays.length  > 0)
+                                         || (area.state.sensors?.alarm_high && area.setup.high.relays.length > 0)}"
                         class:d-none="{!area.state.sensors.alarm}"> </i>
                   {:else}
                     {#each ['day', 'night', 'low', 'high'] as period}
