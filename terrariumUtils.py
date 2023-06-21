5d766ac98b483cb09027cce5e262374e77ed1e4e
TheYOSH <github.com@theyosh.nl>
2023-06-21 23:13:50 +0200

    Add docker check

--- terrariumUtils.py
+++ terrariumUtils.py
@@ -482,3 +482,11 @@ class terrariumUtils:
         # Load language
         trans = gettext.translation("terrariumpi", "locales/", languages=(lang.replace("-", "_"),))
         return trans.gettext
+
+    @staticmethod
+    def is_docker():
+        path = '/proc/self/cgroup'
+        return (
+          os.path.exists('/.dockerenv') or
+          os.path.isfile(path) and any('docker' in line for line in open(path))
+        )
