9f8b18b539015b6fcdbdda691a3df64fb4f8d1f3
TheYOSH <theyosh@users.noreply.github.com>
2023-10-24 18:39:23 +0000

    New Svelte GUI build

--- public/js/fireworks.js
+++ public/js/fireworks.js
@@ -4,12 +4,14 @@
 // when animating on canvas, it is best to use requestAnimationFrame instead of setTimeout or setInterval
 // not supported in all browsers though and sometimes needs a prefix, so we need a shim
 window.requestAnimFrame = (function () {
-  return window.requestAnimationFrame ||
+  return (
+    window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     function (callback) {
       window.setTimeout(callback, 1000 / 60);
-    };
+    }
+  );
 })();
 
 // now we will setup our basic variables for the demo
