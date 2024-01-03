c276b541907da73e441fdfb6257121ad19cfe328
TheYOSH <github.com@theyosh.nl>
2024-01-03 10:25:43 +0100

    Patch abs()

--- gui/patch.sh
+++ gui/patch.sh
@@ -2,3 +2,6 @@
 
 # Patch wrong .scss files: https://github.com/ColorlibHQ/AdminLTE/issues/4255
 sed -i 's@\&\.bg-#{$name} {@#{if\(\&, "\&.bg-#{\$name}",".bg-#{\$name}"\)} {@' node_modules/admin-lte/build/scss/mixins/*
+
+# Bootstrap old abs() function fix
+sed -i 's@  $dividend: abs($dividend);@  $dividend: math.abs($dividend);@' node_modules/bootstrap/scss/vendor/_rfs.scss
