070ff286aaceab84e5b8b4d0b255c53da152ea98
TheYOSH <github.com@theyosh.nl>
2024-01-29 19:31:03 +0100

    Refactor TAPO P100 relay to use mac address for ID

--- hardware/relay/tapo_p100_relay.py
+++ hardware/relay/tapo_p100_relay.py
@@ -1,13 +1,20 @@
 from . import terrariumRelay
 from terrariumUtils import terrariumUtils
 
-# pip install PyP100
+# pip install git+https://github.com/almottier/TapoP100.git
 from PyP100 import PyP100
-
+from hashlib import md5
 
 class terrariumRelayTAPOP100(terrariumRelay):
     HARDWARE = "tapop100"
     NAME = "TAPO P100/5"
+
+    # This will update the device based in mac address
+    def load_hardware(self):
+        super().load_hardware()
+        # Always overrule the ID generating, as we want to use the MAC as that is unique if the IP address is changing
+        data = self.device.getDeviceInfo()
+        self.id = md5(f"{self.HARDWARE}{data['mac'].lower()}".encode()).hexdigest()
 
     def _load_hardware(self):
         address = self.address.split(",")
