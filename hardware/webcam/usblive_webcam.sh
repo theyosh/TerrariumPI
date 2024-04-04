93e2f09f22bea25d4fe0b22b2defc4cb4a823a83
TheYOSH <github.com@theyosh.nl>
2024-04-04 21:12:46 +0200

    Bookworm does not support hardware encoded ffmpeg anymore.... :(

--- hardware/webcam/usblive_webcam.sh
+++ hardware/webcam/usblive_webcam.sh
@@ -9,6 +9,8 @@ ROTATION=$5
 AWB=$6
 DIR=$7
 
+OS=$(grep -ioP '^VERSION_CODENAME=(\K.*)' /etc/os-release)
+
 # Find the needed programs
 FFMPEG=$(type -p ffmpeg)
 
@@ -61,6 +63,11 @@ elif [[ "${ROTATION}" == "270" ]]; then
   ROTATION_ACTION="transpose=2,"
 fi
 
+CODEC="h264_omx"
+if [ "${OS}" == "bookworm" ]; then
+    CODEC="h264"
+fi
+
 # Start streaming
 "${FFMPEG}" -hide_banner -y -nostdin -f v4l2 -flags +global_header -pix_fmt yuv420p -framerate 30 -video_size "${WIDTH}x${HEIGHT}" -re -i "${DEVICE}" \
- -c:v h264_omx -profile:v 66 -flags:v +global_header -flags +cgop -g 6 -b:v 2000K -vf "${ROTATION_ACTION}format=yuv420p,drawtext=fontfile=/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf:box=1:boxcolor=black:text='${NAME} %{localtime\:%a %d %b %Y @ %H\\\\\:%M\\\\\:%S}':fontsize=14:fontcolor=white@1:x=3:y=3" -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+split_by_time -hls_segment_filename "${DIR}/chunk_%03d.ts" "${DIR}/stream.m3u8"
+ -c:v ${CODEC} -profile:v 66 -flags:v +global_header -flags +cgop -g 6 -b:v 2000K -vf "${ROTATION_ACTION}format=yuv420p,drawtext=fontfile=/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf:box=1:boxcolor=black:text='${NAME} %{localtime\:%a %d %b %Y @ %H\\\\\:%M\\\\\:%S}':fontsize=14:fontcolor=white@1:x=3:y=3" -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+split_by_time -hls_segment_filename "${DIR}/chunk_%03d.ts" "${DIR}/stream.m3u8"
