e9ba0df1155f7de48b570ae2a4f6913160a17d39
TheYOSH <github.com@theyosh.nl>
2024-04-29 20:53:41 +0200

    Remove make

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ elif [ "${OS}" == "bullseye" ]; then
 
 elif [ "${OS}" == "bookworm" ]; then
   # We use the python3-opencv from the OS, as piwheels does not provide a compiled package
-  OPENCV_PACKAGES="libopenexr-3-1-30 libilmbase25 liblapack3 libatlas3-base python3-opencv libglib2.0-dev libbluetooth-dev make"
+  OPENCV_PACKAGES="libopenexr-3-1-30 libilmbase25 liblapack3 libatlas3-base python3-opencv libglib2.0-dev libbluetooth-dev"
 
   # Python package version difference per OS
   # On bookworm we use the OS package versions
