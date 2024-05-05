7f54f6c819f881fc2553a400ae50189a1941af55
TheYOSH <github.com@theyosh.nl>
2024-05-05 14:21:30 +0200

    Update translation url

--- gui/constants/urls.js
+++ gui/constants/urls.js
@@ -6,4 +6,4 @@ export const ApiUrl = removeAllTrailingChars(process.env.API_URL || AppUrl);
 export const WebsocketUrl = `${ApiUrl}/live/`.replace(/http/gm, 'ws');
 
 // https://datatables.net/plug-ins/i18n/
-export const DataTablesLanguageUrl = 'https://cdn.datatables.net/plug-ins/2.0.3/i18n';
+export const DataTablesLanguageUrl = 'https://cdn.datatables.net/plug-ins/2.0.6/i18n';
