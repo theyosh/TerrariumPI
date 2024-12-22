59777fbfdabc5e0fe8984355cee58125d6248664
TheYOSH <theyosh@users.noreply.github.com>
2024-12-22 13:59:05 +0000

    New Svelte GUI build

--- gui/modals/SensorFormModal.svelte
+++ gui/modals/SensorFormModal.svelte
@@ -360,7 +360,10 @@
               })}"
             />
           </div>
-          <div class="col-6 col-sm-6 col-md-6 col-lg-2" class:d-none="{!(hardware_type === 'chirp' && sensor_type === 'moisture')}">
+          <div
+            class="col-6 col-sm-6 col-md-6 col-lg-2"
+            class:d-none="{!(hardware_type === 'chirp' && sensor_type === 'moisture')}"
+          >
             <Field
               type="number"
               name="calibration.chirp_min_moist"
@@ -377,7 +380,10 @@
               })}"
             />
           </div>
-          <div class="col-6 col-sm-6 col-md-6 col-lg-2" class:d-none="{!(hardware_type === 'chirp' && sensor_type === 'moisture')}">
+          <div
+            class="col-6 col-sm-6 col-md-6 col-lg-2"
+            class:d-none="{!(hardware_type === 'chirp' && sensor_type === 'moisture')}"
+          >
             <Field
               type="number"
               name="calibration.chirp_max_moist"
