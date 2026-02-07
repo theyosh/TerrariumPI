5dd014f3406b6cbbd0ea48e964b34a42c3b49285
TheYOSH <github.com@theyosh.nl>
2026-02-07 10:47:38 +0100

    Refactor db_cleanup

--- contrib/db_cleanup.py
+++ contrib/db_cleanup.py
@@ -7,8 +7,7 @@ Run this script in the contrib folder with the same python version as TerrariumP
 
 import sqlite3
 from datetime import datetime, timedelta
-import time
-import math
+from time import time
 import shutil
 import os
 import requests
@@ -36,19 +35,23 @@ class HistoryCleanup:
         """
 
         print(
-            f"This script will cleanup your terrariumpi.db file. We will keep {period} of data from now. If you want to make a backup first, please enter no and make your backup."
+            f"This script will cleanup your terrariumpi.db file. We will keep {period.days} days of data from now. If you want to make a backup first, please enter no and make your backup."
         )
+
+        self.cleanup_tables = ["Button","Sensor"]
+        self.progress_amount = 100000
+        self.total_to_keep = 0
+        self.counter = 0
+
+        self.database = database
+        self.database_size = 0
+        self.time_limit = f"{datetime.now() - period:%Y-%m-%d %H:%M:%S}"
 
         self.check_offline()
 
-        self.delete_batch = batch
-        self.time_limit = f"{datetime.now() - period:%Y-%m-%d %H:%M:%S}"
-
-        self.database = database
-        self.new_database = self.database.replace(".db", ".new.db")
-
         self.db = sqlite3.connect(self.database)
         with self.db as db:
+            db.set_progress_handler(self.__sql_progress, self.progress_amount)
             cur = db.cursor()
             cur.execute("PRAGMA journal_mode = OFF")
             cur.execute("PRAGMA temp_store = OFF")
@@ -63,12 +66,17 @@ class HistoryCleanup:
             self.version = int(cur.execute("PRAGMA user_version").fetchall()[0][0])
 
     def check_free_storage(self):
-        diskstats = shutil.disk_usage(self.database)
-        filesize = os.path.getsize(self.database)
-        print("Database size: {}, free diskspace: {}".format(humansize(filesize), humansize(diskstats[2])))
-        if filesize > diskstats[2]:
-            print("Not enough space left for cleaning the database. We need at least {}".format(humansize(filesize)))
+        disk_stats = shutil.disk_usage(self.database)
+        file_size = os.path.getsize(self.database)
+        print(f"Database size: {humansize(file_size)}, free disk space: {humansize(disk_stats[2])}")
+        if file_size > disk_stats[2]:
+            print(f"Not enough disk space left for cleaning the database. We need at least {humansize(file_size)}")
             exit(1)
+
+        if self.database_size == 0:
+            self.database_size = file_size
+        else:
+            print(f"Cleaned up {humansize(self.database_size - file_size)}")
 
     def check_offline(self):
         try:
@@ -79,10 +87,11 @@ class HistoryCleanup:
         except requests.ConnectionError:
             pass
 
-    def get_total_records(self, table, period=None):
-        sql = f"SELECT count(*) as total, MIN(timestamp) as begin, MAX(timestamp) as end FROM {table}"
+    def get_total_records(self, table, keep=False):
+        self.counter = 0
+        sql = f"SELECT count(*) as total, MIN(timestamp) as begin, MAX(timestamp) as end FROM `{table}`"
         parameters = ()
-        if period is not None:
+        if keep:
             sql += " WHERE timestamp < ?"
             parameters = (self.time_limit,)
 
@@ -90,204 +99,58 @@ class HistoryCleanup:
             for row in db.execute(sql, parameters):
                 return (row["total"], row["begin"], row["end"])
 
-    def get_clean_up_records(self, table):
+    def get_records_to_delete(self, table):
         return self.get_total_records(table, True)
 
-    def get_orphan_records(self, table):
-        if "SensorHistory" == table:
-            table_source = "Sensor"
-        if "ButtonHistory" == table:
-            table_source = "Button"
-        elif "RelayHistory" == table:
-            table_source = "Relay"
+    def __sql_progress(self):
+        self.counter += 1
+        # f-string format not working here
+        print("{}".format(['|','/','-','\\'][self.counter % 4]), end="\r")
+        return 0  # Return 0 to continue, non-zero to abort
 
-        sql = f"SELECT count(*) as total, MIN(timestamp) as begin, MAX(timestamp) as end FROM {table} WHERE {table_source.lower()} NOT IN (SELECT id FROM {table_source})"
+    def __vacuum(self):
+        start = time()
 
-        with self.db as db:
-            for row in db.execute(sql):
-                return (row["total"], row["begin"], row["end"])
-
-    def get_space_back(self):
-        start = time.time()
         print(
             "Start reclaiming lost space. This will rebuild the database and give all the delete space back. This will take a lot of time"
         )
-
-        self.db.execute(f"VACUUM INTO '{self.new_database}'")
-
-        with sqlite3.connect(self.new_database) as db:
+        print("  Vacuuming database ...", end="\r")
+        self.counter = 0
+        with self.db as db:
             cur = db.cursor()
+            cur.execute("VACUUM")
             cur.execute("PRAGMA journal_mode = MEMORY")
             cur.execute("PRAGMA temp_store = MEMORY")
             cur.execute("PRAGMA user_version = {}".format(self.version))
 
-        print(f"Done in {(time.time()-start):.2f} seconds")
+        print(f"Vacuuming database done in {(time()-start):.2f} seconds")
 
-    def __delete_orphan_records(self, table):
-        if "SensorHistory" == table:
-            table_source = "Sensor"
-        if "ButtonHistory" == table:
-            table_source = "Button"
-        elif "RelayHistory" == table:
-            table_source = "Relay"
+    def get_space_back(self):
+        self.__clean_up_by_deleting()
+        self.__vacuum()
 
-        print(f"Starting cleaning up table '{table}' by deleting orphan records. This could take some time...")
+    def __clean_up_by_deleting(self):
+        for table in self.cleanup_tables:
+            table += "History"
 
-        start = time.time()
-        print("Analyzing total rows:      ", end="", flush=True)
-        total_sensor_data = self.get_total_records(table)
-        number_padding = len(str(total_sensor_data[0]))
-        if total_sensor_data[0] == 0:
-            print(
-                " {}. Took {:.2f} seconds".format(
-                    str(total_sensor_data[0]).rjust(number_padding),
-                    time.time() - start,
-                )
-            )
-            return
+            print(f"Start cleaning up table {table} ...")
+            print("  Getting totals ...", end="\r")
+            totals = self.get_total_records(table)
+            print("  Getting records to delete ...", end="\r")
+            cleanup = self.get_records_to_delete(table)
+            print(f"Table {table} contains {totals[0]} records, of which being deleted {cleanup[0]} records {cleanup[0]/totals[0]*100:.2f}%.")
 
-        print(
-            " {} from {:%Y-%m-%d %H:%M} till {:%Y-%m-%d %H:%M}. Took {:.2f} seconds".format(
-                str(total_sensor_data[0]).rjust(number_padding),
-                datetime.fromisoformat(total_sensor_data[1]),
-                datetime.fromisoformat(total_sensor_data[2]),
-                time.time() - start,
-            )
-        )
-        start = time.time()
-        print("Analyzing rows to clean up:", end="", flush=True)
-        total_cleanup_data = self.get_orphan_records(table)
+            sql = f"DELETE FROM `{table}` WHERE timestamp < ?"
+            parameters = (self.time_limit,)
+            print("  Deleting data ...", end="\r")
+            start = time()
+            self.counter = 0
 
-        if total_cleanup_data[0] == 0:
-            print(" No data, nothing to do!")
-            return
+            with self.db as db:
+                cur = db.cursor()
+                cur.execute(sql, parameters)
 
-        print(
-            " {} from {:%Y-%m-%d %H:%M} till {:%Y-%m-%d %H:%M}. Took {:.2f} seconds".format(
-                str(total_cleanup_data[0]).rjust(number_padding),
-                datetime.fromisoformat(total_cleanup_data[1]),
-                datetime.fromisoformat(total_cleanup_data[2]),
-                time.time() - start,
-            )
-        )
-
-        start = time.time()
-        delete_steps = math.ceil(total_cleanup_data[0] / self.delete_batch)
-        print(
-            "Removing {} rows ({:.2f}%) of data in {} steps of {} rows.".format(
-                total_cleanup_data[0],
-                (total_cleanup_data[0] / total_sensor_data[0]) * 100,
-                delete_steps,
-                self.delete_batch,
-            )
-        )
-        print("0%", end="", flush=True)
-        new_percentage = 10
-
-        sql = f"DELETE FROM {table} WHERE {table_source.lower()} NOT IN (SELECT id FROM {table_source}) LIMIT ?"
-        parameters = (self.delete_batch,)
-
-        with self.db as db:
-            for step in range(delete_steps):
-                if int((step / delete_steps) * 100) >= new_percentage:
-                    print("{:.0f}%".format(new_percentage), end="", flush=True)
-                    new_percentage += 10
-
-                print(".", end="", flush=True)
-                db.execute(sql, parameters)
-
-        print("100%")
-        print("Clean up is done in {:.2f} seconds".format(time.time() - start))
-
-    def __clean_up(self, table):
-        print(
-            f"Starting cleaning up table '{table}'. Deleting data older then {self.time_limit} in batches of {self.delete_batch} records. This could take some time..."
-        )
-
-        start = time.time()
-        print("Analyzing total rows:      ", end="", flush=True)
-        total_sensor_data = self.get_total_records(table)
-        number_padding = len(str(total_sensor_data[0]))
-        if total_sensor_data[0] == 0:
-            print(
-                " {}. Took {:.2f} seconds".format(
-                    str(total_sensor_data[0]).rjust(number_padding),
-                    time.time() - start,
-                )
-            )
-            return
-
-        print(
-            " {} from {:%Y-%m-%d %H:%M} till {:%Y-%m-%d %H:%M}. Took {:.2f} seconds".format(
-                str(total_sensor_data[0]).rjust(number_padding),
-                datetime.fromisoformat(total_sensor_data[1]),
-                datetime.fromisoformat(total_sensor_data[2]),
-                time.time() - start,
-            )
-        )
-        start = time.time()
-        print("Analyzing rows to clean up:", end="", flush=True)
-        total_cleanup_data = self.get_clean_up_records(table)
-
-        if total_cleanup_data[0] == 0:
-            print(" No data, nothing to do!")
-            return
-
-        print(
-            " {} from {:%Y-%m-%d %H:%M} till {:%Y-%m-%d %H:%M}. Took {:.2f} seconds".format(
-                str(total_cleanup_data[0]).rjust(number_padding),
-                datetime.fromisoformat(total_cleanup_data[1]),
-                datetime.fromisoformat(total_cleanup_data[2]),
-                time.time() - start,
-            )
-        )
-
-        start = time.time()
-        delete_steps = math.ceil(total_cleanup_data[0] / self.delete_batch)
-        print(
-            "Removing {} rows ({:.2f}%) of data in {} steps of {} rows.".format(
-                total_cleanup_data[0],
-                (total_cleanup_data[0] / total_sensor_data[0]) * 100,
-                delete_steps,
-                self.delete_batch,
-            )
-        )
-        print("0%", end="", flush=True)
-        new_percentage = 10
-
-        sql = f"DELETE FROM {table} WHERE timestamp < ? LIMIT ?"
-        parameters = (self.time_limit, self.delete_batch)
-
-        with self.db as db:
-            for step in range(delete_steps):
-                if int((step / delete_steps) * 100) >= new_percentage:
-                    print("{:.0f}%".format(new_percentage), end="", flush=True)
-                    new_percentage += 10
-
-                print(".", end="", flush=True)
-                db.execute(sql, parameters)
-
-        print("100%")
-        print("Clean up is done in {:.2f} seconds".format(time.time() - start))
-
-    def move_db(self):
-        shutil.move(self.database, self.database.replace(".db", ".db.old"))
-        shutil.move(self.new_database, self.database)
-
-    def delete_orphan_sensors(self):
-        self.__delete_orphan_records("SensorHistory")
-
-    def delete_orphan_doors(self):
-        self.__delete_orphan_records("ButtonHistory")
-
-    def delete_orphan_relays(self):
-        self.__delete_orphan_records("RelayHistory")
-
-    def clean_up_sensors(self):
-        self.__clean_up("SensorHistory")
-
-    def clean_up_doors(self):
-        self.__clean_up("ButtonHistory")
+            print(f"Deleting data done in {(time()-start):.2f} seconds")
 
 
 cleanup = HistoryCleanup()
@@ -297,20 +160,7 @@ go = input("Would you like to continue? Enter yes to start. Anything else will a
 if go.lower() != "yes":
     exit(0)
 
-cleanup.delete_orphan_doors()
-cleanup.delete_orphan_relays()
-cleanup.delete_orphan_sensors()
-
-cleanup.clean_up_sensors()
-cleanup.clean_up_doors()
-
 cleanup.get_space_back()
-cleanup.move_db()
 
-print("Database is now cleaned and should be reduced in size:")
+print("Database is now cleaned and should be reduced in size. TerrariumPI can now be started.")
 cleanup.check_free_storage()
-print(
-    "Restart TerrariumPI and check if the sensor graphs still working. If it is al working, remove the file {}".format(
-        cleanup.database.replace(".db", ".db.old")
-    )
-)
